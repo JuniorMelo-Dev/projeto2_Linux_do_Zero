9e81bcd8398c1d2ac244916b5cd75fc32b44e2e6
JuniorMelo-Dev <juniormelo.dev@gmail.com>
2023-02-17 22:06:21 +0000

    Servidor Web Apache

---
+++ script-iac2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Atualizando Servidor"
+
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install unzip -y
+
+echo "Baixando e copiando arquivos da aplicação"
+
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linuz-site-dio-main
+cp -R * /var/www/html/
+
